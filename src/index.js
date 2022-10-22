86013828be48bc2578b537785fa40a546e9079bf
Napowastaken <Napowastaken@users.noreply.github.com>
2022-10-22 12:52:22 -0300

    i was about to do this change but i forgor

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+if (require('fs').existsSync('./node_modules/dotenv')) require('dotenv').config();
 const Client = require('./utils/classes/Client.js');
 new Client().start();
 process.on('uncaughtException', err => console.log(err));
